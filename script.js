cb35365c0d2c1d3840f5bc06242b3a23f0b54ab9
Billy Schulze <billyschulze97@gmail.com>
2017-05-21 06:57:02 +1000

    Added trees and orbs

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const MOVE_SPEED = 200.0;
 const SPRINT_MODIFIER = 2;
 const MOVE_REDUCTION = 10.0;
 
+const ORB_ROT_SPEED = 5;
+
 // ==============================================================
 // resources
 var soundShootFile = "resources/sounds/bow-release.wav";
@@ -39,6 +41,7 @@ var controls;
 var geometry, material, mesh, loader;
 var floor;
 var objects;
+var trees, orbs;
 
 // renderer
 var renderer;
@@ -66,7 +69,7 @@ function init() {
 
     // setup scene
     scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(0xffffff, 0, 750);
+    scene.fog = new THREE.Fog(0x010101, 0, 750);
 
     // setup mouse controls
     pointerLockInit();
@@ -74,19 +77,25 @@ function init() {
     // setup audio
     audioInit();
 
-    // setup lights
-    lightInit();
-
     // setup meshes
     objects = [];
     meshesInit();
 
+    // create trees and orbs
+    trees = [];
+    orbs = [];
+    createTrees(100);
+
+    // setup lights
+    lightInit();
+
     // setup renderer
     renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor(0xffffff);
+    renderer.setClearColor(0x010101);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.type = THREE.BasicShadowMap;
+    renderer.shadowMap.enabled = true;
     document.body.appendChild(renderer.domElement);
 
     // setup other
@@ -162,8 +171,17 @@ function audioInit() {
 
 function lightInit() {
     // TODO make lights look better, shadows, etc
-    light = new THREE.AmbientLight(0x404040);
+    light = new THREE.AmbientLight(0x404040, 0.3);
     scene.add(light);
+
+    light = new THREE.PointLight(0xffffff, 1, 100);
+    light.position.set(0, 30, 0);
+    light.castShadow = true;
+    light.shadow.camera.near = 0.3;
+    light.shadow.camera.far = 50;
+    scene.add(light);
+
+
 }
 
 function meshesInit() {
@@ -171,6 +189,57 @@ function meshesInit() {
     createTestingBox();
 
     createBow();
+}
+
+function createTrees(radius) {
+    var files = ["./tree1.json", "./tree1.json", "./tree1.json", "./tree1.json", "./tree1.json"];
+    for (var i = 0; i < files.length; i++) {
+        var theta = i * 2 * Math.PI / files.length;
+        var x = radius * Math.cos(theta);
+        var y = radius * Math.sin(theta);
+        createTree(files[i], x, y);
+        createOrb(new THREE.Color().setHSL(Math.random(), 1, 0.8), x, y);
+    }
+}
+
+function createTree(file, x, y) {
+    var loader = new THREE.JSONLoader();
+    loader.load(file, function(geometry, materials) {
+        var material = new THREE.MeshPhongMaterial({
+            color: 0x8B4513
+        });
+        var tree = new THREE.Mesh(geometry, material);
+        tree.castShadow = true;
+        tree.position.set(x, 0, y);
+        tree.rotateY(Math.random() * 2 * Math.PI);
+        scene.add(tree);
+        trees.push(tree);
+        objects.push(tree);
+    })
+}
+
+function createOrb(color, x, y) { // 0xffec89
+    var bulbGeometry = new THREE.TetrahedronGeometry(10, 0);
+    var bulbLight = new THREE.PointLight(color, 1, 100, 1 );
+    var bulbMat = new THREE.MeshStandardMaterial( {
+        emissive: color,
+        emissiveIntensity: 1,
+        color: 0x000000
+    });
+    var orb = new THREE.Mesh( bulbGeometry, bulbMat );
+    bulbLight.add(orb);
+    bulbLight.position.set(x, 60, y);
+    bulbLight.castShadow = true;
+    scene.add(bulbLight);
+    orbs.push({
+        object: orb,
+        rot: {
+            x: Math.random() * ORB_ROT_SPEED,
+            y: Math.random() * ORB_ROT_SPEED,
+            z: Math.random() * ORB_ROT_SPEED
+        }
+    });
+    objects.push(orb)
 }
 
 function createBow() {
@@ -192,7 +261,7 @@ function loadBow(geometry, materials) {
         metalness: 1.0,
         skinning: true,
         side: THREE.DoubleSide,
-        shading: THREE.SmoothShading,
+        shading: THREE.FlatShading,
         envMap: reflectionCube,
         envMapIntensity: 0.3
     });
@@ -205,45 +274,71 @@ function loadBow(geometry, materials) {
 }
 
 function createFloorPlane() {
-    var geo = new THREE.PlaneGeometry(200,200,100,100);
-    var tloader = new THREE.TextureLoader();
-    var dloader = new THREE.TextureLoader();
-    var nloader = new THREE.TextureLoader();
-    dloader.load( 'resources/textures/grass_DISP.png', function (displacementMap) {
-        nloader.load( 'resources/textures/grass_NRM.png', function (normalMap) {
-            tloader.load(grassFile, function (texture) {
-                texture.wrapS = THREE.RepeatWrapping;
-                texture.wrapT = THREE.RepeatWrapping;
-                texture.repeat.x = 10;
-                texture.repeat.y = 10;
-                var mat = new THREE.MeshPhongMaterial({
-                    map: texture,
-                    displacementMap: displacementMap,
-                    displacementScale: 2,
-                    normalMap: normalMap
-                });
-                geo.rotateX(-Math.PI/2);
-                // for (var i = 0, l = geometry.faces.length; i < l; i++) {
-                //     var face = geometry.faces[i];
-                //     face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-                //     face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-                //     face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-                // }
-                // material = new THREE.MeshPhongMaterial({vertexColors: THREE.VertexColors});
-                floor = new THREE.Mesh(geo, mat);
-                floor.castShadow = true;
-                floor.receiveShadow = true;
-                scene.add(floor);
-            });
-        });
-    });
+    // var geo = new THREE.PlaneGeometry(200,200,100,100);
+    // var tloader = new THREE.TextureLoader();
+    // var dloader = new THREE.TextureLoader();
+    // var nloader = new THREE.TextureLoader();
+    // dloader.load( 'resources/textures/grass_DISP.png', function (displacementMap) {
+    //     displacementMap.wrapS = THREE.RepeatWrapping;
+    //     displacementMap.wrapT = THREE.RepeatWrapping;
+    //     displacementMap.repeat.x = 10;
+    //     displacementMap.repeat.y = 10;
+    //     nloader.load( 'resources/textures/grass_NRM.png', function (normalMap) {
+    //         normalMap.wrapS = THREE.RepeatWrapping;
+    //         normalMap.wrapT = THREE.RepeatWrapping;
+    //         normalMap.repeat.x = 10;
+    //         normalMap.repeat.y = 10;
+    //         tloader.load(grassFile, function (texture) {
+    //             texture.wrapS = THREE.RepeatWrapping;
+    //             texture.wrapT = THREE.RepeatWrapping;
+    //             texture.repeat.x = 10;
+    //             texture.repeat.y = 10;
+    //             var mat = new THREE.MeshPhongMaterial({
+    //                 map: texture,
+    //                 displacementMap: displacementMap,
+    //                 displacementScale: 2,
+    //                 normalMap: normalMap
+    //             });
+    //             geo.rotateX(-Math.PI/2);
+    //             // for (var i = 0, l = geometry.faces.length; i < l; i++) {
+    //             //     var face = geometry.faces[i];
+    //             //     face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+    //             //     face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+    //             //     face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+    //             // }
+    //             // material = new THREE.MeshPhongMaterial({vertexColors: THREE.VertexColors});
+    //             floor = new THREE.Mesh(geo, mat);
+    //             floor.castShadow = true;
+    //             floor.receiveShadow = true;
+    //             scene.add(floor);
+    //         });
+    //     });
+    // });
+    geometry = new THREE.PlaneGeometry(500,500,200,200);
+    geometry.rotateX(-Math.PI/2);
+    for (var i = 0, l = geometry.faces.length; i < l; i++) {
+        var face = geometry.faces[i];
+        face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.15 + 0.35, 0.75, Math.random() * 0.45 + 0.15);
+        face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.15 + 0.35, 0.75, Math.random() * 0.45 + 0.15);
+        face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.15 + 0.35, 0.75, Math.random() * 0.45 + 0.15);
+    }
+    for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
+        var vertex = geometry.vertices[ i ];
+        vertex.x += Math.random() * 2 - 1;
+        vertex.y += Math.random() * 2;
+        vertex.z += Math.random() * 2 - 1;
+    }
+    material = new THREE.MeshPhongMaterial({vertexColors: THREE.VertexColors});
+    floor = new THREE.Mesh(geometry, material);
+    floor.receiveShadow = true;
+    scene.add(floor);
 }
 
 function createTestingBox() {
-    geometry = new THREE.BoxGeometry(50, 50, 50);
+    geometry = new THREE.BoxGeometry(10, 10, 10);
     material = new THREE.MeshPhongMaterial({color: 0x00ff00});
     var cube = new THREE.Mesh(geometry, material);
-    cube.position.set(0,30,0);
+    cube.position.set(0,10,0);
     cube.castShadow = true;
     cube.receiveShadow = true;
     scene.add(cube);
@@ -403,6 +498,46 @@ function update() {
     controls.getObject().translateZ(velocity.z * delta);
     controls.getObject().translateX(velocity.x * delta);
 
+    // distort orbs
+    // distortTetrahedron(orbs[0], 250*delta, 250);
+    for (var i = 0; i < orbs.length; i++) {
+        orbs[i].object.rotation.x += orbs[i].rot.x * delta;
+        orbs[i].object.rotation.y += orbs[i].rot.y * delta;
+        orbs[i].object.rotation.z += orbs[i].rot.z * delta;
+    }
+
+
 
     time = now;
+}
+
+function glitchSphere(radius, widthSegments, heightSegments, randomness) {
+    var geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+    for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
+        var v = geometry.vertices[ i ];
+        var dist = Math.pow(v.x, 2) + Math.pow(v.y, 2) + Math.pow(v.z, 2);
+        console.log(dist);
+        // vertex.x += Math.random() * randomness - randomness/2;
+        // vertex.y += Math.random() * randomness - randomness/2;
+        // vertex.z += Math.random() * randomness - randomness/2;
+    }
+    return geometry;
+}
+
+function distortTetrahedron(geometry, randomness, max) {
+    for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
+        var v = geometry.vertices[i];
+        var dist = 0;
+        var x, y, z;
+        do {
+            var x = v.x + Math.random() * randomness - randomness/2;
+            var y = v.y + Math.random() * randomness - randomness/2;
+            var z = v.z + Math.random() * randomness - randomness/2;
+            dist = Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2);
+        } while (dist > max);
+        v.x = x;
+        v.y = y;
+        v.z = z;
+    }
+    geometry.verticesNeedUpdate = true;
 }
